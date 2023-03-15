2b72853429f25bd5e574047b1c4da207b571ef8c
Luiz Langkammer <luizeduardo112001@gmail.com>
2023-03-15 16:20:19 -0300

    initial folder and class structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import aplication.abstraction.domain.Individual;
+import aplication.abstraction.factory.IIndividualFactory;
+import aplication.implementation.domain.X1X2Individual;
+import aplication.implementation.factory.IndividualFactory;
+import domain.EnumIndividualTypes;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        IIndividualFactory individualFactory = new IndividualFactory();
+        int generations = 100;
+        int populationDimension = 20;
+
+        Individual[] initialPopulation = new Individual[populationDimension];
+        for(int i=0; i< initialPopulation.length; i++){
+            initialPopulation[i] = individualFactory.obtem(EnumIndividualTypes.X1X2);
+        }
+
+
+
+
     }
 }
